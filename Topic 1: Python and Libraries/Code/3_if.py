52d7bdd13998d3c735963d7e7b8a81423568475b
Devin-Wong <wangjin.devin@gmail.com>
2024-01-16 13:05:22 -0500

    U

---
+++ Topic 1: Python and Libraries/Code/3_if.py
@@ -0,0 +1,19 @@
+# a = 0
+
+# if a>0:
+#     print("It is a positive number")
+# elif a<0:
+#     print("It is a negative number")    
+# else:
+#     print("It is zero.")    
+
+a = -2
+
+b = True if a>0 else False
+print(b)
+
+# if a<0:
+#     b = True
+# else:
+#     b = False
+
