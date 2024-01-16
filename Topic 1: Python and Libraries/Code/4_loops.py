0fb6932ca5d7320c4b5e04f20bf7bbe76874dc2c
Devin-Wong <wangjin.devin@gmail.com>
2024-01-16 13:17:56 -0500

    U

---
+++ Topic 1: Python and Libraries/Code/4_loops.py
@@ -0,0 +1,13 @@
+# print 'hello' 3 times
+# n = 3
+
+## ----- 1. while loop
+# i = 0
+# while i<3:
+#     print('Hello')
+#     i += 1 # equals i = i + 1
+
+## ------ 2. for loop
+for i in range(3):
+    print('Hello')
+
