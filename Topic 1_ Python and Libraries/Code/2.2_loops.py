6bdbb91e655fd4cc904bdea51354b40e8e022f67
Piyaporn Puangprasert <dngooddeals@gmail.com>
2024-01-23 11:46:04 -0500

    learned about map + lambda

--- Topic 1_ Python and Libraries/Code/2.2_loops.py
+++ Topic 1_ Python and Libraries/Code/2.2_loops.py
@@ -44,3 +44,47 @@ list_1 = [20, 30, 50 ]
 list_2 = ['Rutgers', 'NJ', 2, 3]
 m = [[2,0], [2,3], 5]
 print(m[0][1])
+'''
+#a = m.append(5) # print(a) shows "none"
+## m.add(50) # list cannot use "add"
+print(m)
+list_1.sort()
+print('list_1',list_1)
+list_1.sort(reverse = True)
+print('list_1 reverse',list_1)
+m.extend(list_2)
+print('m.extend(list_2)', m)
+b = list_1 + list_2
+print('b = list_1 + list_2',b)
+'''
+#------- in ---------------- 'lists' are mulable vs 'string' is not 
+s = [1, 2]
+print(1 in s)
+print(3 in s)
+
+# #-------------- map tranditional for loop ----------------------
+n = [ 1, 2, 3 , 4]
+# c =[]
+# for i in range(len(n)):
+#     d = n[i]
+#     square = d**2
+#     c.append(square)
+# print(c)
+
+#-------------------------- MAP fuction ----------------
+def square_value(x):
+    return x ** 2
+
+square = map(square_value, n)
+print(square) # print <map object at 0x0000026FAADDD090>
+
+square = list(square)
+print('list(square)',square) 
+
+# lamda function 
+square = lambda x: x**3
+s = square(2)
+print(' 2x2x2 =',s)
+
+#--------------- map + lambda fuction
+
