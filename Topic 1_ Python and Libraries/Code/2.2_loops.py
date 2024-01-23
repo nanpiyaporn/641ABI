70891ee7d0209c47311a590c448aef464ca957ab
Piyaporn Puangprasert <dngooddeals@gmail.com>
2024-01-23 11:05:19 -0500

    add string.py

---
+++ Topic 1_ Python and Libraries/Code/2.2_loops.py
@@ -0,0 +1,46 @@
+# print 'hello' 3 times
+# n = 3
+
+## ----- 1. while loop
+# i = 0
+# while i<3:
+#     print('Hello')
+#     i += 1 # equals i = i + 1
+
+## ------ 2. for loop
+# for i in range(3):
+#     print('Hello')
+
+# for i in range(4):
+#     if i == 1:
+#         continue
+#     print(i)
+# s = "Hello, Jin! kdjfl yej kj, jfk, 08857"
+# # s = s[2:5]
+# print (s)
+# print (len(s)-1)
+# ##                    s[0:2] ==s[:2]
+# ##                      s[-1]  => !
+# ##                      s[-2:0] => n! 
+# ## you can not change the string value
+# ##
+# y= 'h'+s[1:] # how to change the first
+# print (y)
+
+# y.split()
+# print(y)
+# y.split(',')
+# print(y)
+# y.split(', ')
+# print(y)
+
+# # >>> s[:2]
+# # 'He'
+# # >>> s[-1]
+# # '!'
+# # >>> s[-2:]
+# # 'n!'
+list_1 = [20, 30, 50 ]
+list_2 = ['Rutgers', 'NJ', 2, 3]
+m = [[2,0], [2,3], 5]
+print(m[0][1])
