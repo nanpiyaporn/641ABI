d0affdd19f0e81e9eadfe11ebf51270aedb2ee48
Piyaporn Puangprasert <dngooddeals@gmail.com>
2024-02-20 12:09:14 -0500

    fetch

--- string.py
+++ string.py
@@ -1,28 +1,58 @@
 # Jan 16, 2024
-name = "Nan"
-number = 3
-x = 5
-y = 8
+#name = "Nan"
+# number = 3
+#x = 5
+# = 8
 
 
-z = x +y
-print("----------------------")
-print(f"Hello {name},  \ncalculate: {x} + {y} = {z}")
+#z = x +y
+#print("----------------------")
+#print(f"Hello {name},  \ncalculate: {x} + {y} = {z}")
 
 
-def run(name):
-  print("Hello,", name)
+#def run(name):
+#  print("Hello,", name)
 
-def add(a,b):
-  c = a + b
-  return c
+#def add(a,b):
+#  c = a + b
+#  return c
 
-add1 = add( 14, 5)
+#add1 = add( 14, 5)
 
-print(add1)
-run(name)
+#print(add1)
+#run(name)
 ## ---3 function with default value w/o return value
+#Conditional statement: : if statement
+
+# print("----------------------")
+# a = -5
+# print(a)
+# if a> 0:
+#   print("It is a position number")
+# elif a<0:
+#   print("It is a negative number")
+# elif a==0:
+#   print("It is zero.")
 
 
 
-print("----------------------")
+# print("----------------------")
+# b = 1
+# c = True if b > 0 else False
+# print(b)
+# print (c)
+# print("----------------------")
+# loop print 3 time
+n = 3
+i = 0
+while i < n:
+  print("Hello", i)
+  i = i +1
+
+## 2.for loop
+for j in range(3):
+  print("Hi", j)
+  print(list(range(3)))
+  j = j +1
+## break
+## String and Lists
